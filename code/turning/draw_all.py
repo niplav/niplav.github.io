4d834817667d0985e5e3ea255fc7aedbbc367188
niplav <niplav@posteo.net>
2023-08-21 20:10:06 +0200

    Add.

--- code/turning/draw_all.py
+++ code/turning/draw_all.py
@@ -2,6 +2,34 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+fig=plt.figure(figsize=(10,10))
+
+df=pd.read_csv('./hodge_preservations.csv', header=None)
+summ=df.groupby(0).describe()
+preserves=df.loc[df[1]==1.0].groupby(0).count()[1]
+
+#plt.plot(summ[2]['mean'], color="green", label="Mean |imcs(G)|")
+
+plt.plot(summ[1]['mean'], color="red", label="Mean amsp(G) (hodgeresolve)", linewidth=3)
+plt.plot(preserves/summ[1]['count'], color="pink", label="Graphs with amsp(G)=1 (hodgeresolve)", linewidth=3)
+plt.plot(summ[1]['min'], color="brown", label="Min amsp(G) (hodgeresolve)", linewidth=3)
+
+df=pd.read_csv('./eged_preservations.csv', header=None)
+summ=df.groupby(0).describe()
+preserves=df.loc[df[1]==1.0].groupby(0).count()[1]
+
+plt.plot(summ[1]['mean'], color="blue", label="Mean amsp(G) (eged)", linewidth=3)
+plt.plot(preserves/summ[1]['count'], color="cyan", label="Graphs with amsp(G)=1 (eged)", linewidth=3)
+plt.plot(summ[1]['min'], color="darkblue", label="Min amsp(G) (eged)", linewidth=3)
+
+plt.legend()
+
+plt.xlabel('Number of nodes in graph')
+plt.ylabel('Percentage')
+plt.xticks()
+
+plt.savefig("preservations.png")
+
 def plot_confusions(df, multiplier):
 	values=np.zeros(len(labels))
 	for i in range(0, len(labels)):
@@ -19,7 +47,7 @@ x=np.arange(len(labels))
 width=1/5
 multiplier=0
 
-fig, ax=plt.subplots(constrained_layout=True)
+fig, ax=plt.subplots(constrained_layout=True, figsize=(10,10))
 
 for c in list(confusions.groupby(0)):
 	plot_confusions(c[1], c[0])
